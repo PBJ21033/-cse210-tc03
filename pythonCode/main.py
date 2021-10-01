588e150bee5de96d49505cdaefe97b4c7efb76a4
Jadan White <34385383+Retrnon@users.noreply.github.com>
2021-10-01 12:55:43 -0600

    updating main
    added if name is main clause and several imports

--- pythonCode/main.py
+++ pythonCode/main.py
@@ -1,6 +1,20 @@
 from jones import hello_trevin
+from lespin import hello_lespin
+from white import jadan_greeting
 
-print('Welcome to our Callaboration')
+if __name__ == "__main__":
 
-# Hello from Trevin
-hello_trevin()
+    print('Welcome to our Callaboration')
+
+    # Hello from Trevin
+    hello_trevin()
+
+    # Hello from Carlos
+    hello_lespin()
+
+    # Hello from Jadan
+    jadan_greeting()
+
+    # Hello from Devin
+
+    # Hello from Peter
